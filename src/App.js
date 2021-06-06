ccecfe543f4e6023f458de6843b97b8e212bb0f6
Lisa Wu <lisawu0119@gmail.com>
2021-06-05 20:47:11 -0500

    added cards and search function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import MovieInfo from './components/MovieInfo';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MovieInfo />
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,2 +1,47 @@
-import React, {useState} from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardContent, CardMedia, Grid } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles((theme) => ({
+  card: {
+    height: "500px",
+    width: "300px",
+  },
+  img: {
+    height: "350px",
+    width: "250px",
+    margin: "auto",
+  },
+}));
+
+export default function Movies(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      {props.movies.map((movies) => {
+        return (
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+          >
+            <Grid item>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography gutterBottom variant="h9" component="h3">
+                    {movies.Title}
+                  </Typography>
+                  <CardMedia className={classes.img} image={movies.Poster} />
+                  {movies.Year}
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        );
+      })}
+    </div>
+  );
+}
